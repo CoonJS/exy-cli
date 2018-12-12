0c4d8c873d0bc99e4de8faae7114dd1c3cf4ac18
k.tribunsky <k.tribunsky@bi.zone>
2018-12-12 14:05:18 +0300

    added storybook template

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const shell = require('shelljs');
 const Spinner = require('cli-spinner').Spinner;
 
 const questions = require('./questions');
-const templatePath = 'src/ReactApp';
+const getTemplatePath = template => `src/${template}`;
 
 clear();
 
@@ -29,26 +29,35 @@ program
 program
   .command('init')
   .action(prompt(questions).then(answers => {
-    const packagePath = path.resolve(__dirname, templatePath, 'package.json');
+    console.log('answers', answers);
+
+    const packagePath = path.resolve(__dirname, getTemplatePath(answers.template), 'package.json');
 
     fs.readFile(packagePath, 'utf8', (err, data) => {
       const baseConfig = JSON.parse(data);
-      const packageJSONBaseConfig = { ...baseConfig, ...answers };
+      const packageJSONBaseConfig = {
+        ...baseConfig,
+        ...answers
+      };
+
+      const templatePath = getTemplatePath(answers.template);
 
       copyFolderRecursiveSync(path.resolve(__dirname, templatePath), process.cwd());
+
+      fs.renameSync(path.resolve(__dirname, answers.template), path.resolve(__dirname, answers.name));
 
       const spinner = new Spinner('Installing dependencies.. %s');
       spinner.setSpinnerString('|/-\\');
       spinner.start();
 
-      fs.writeFile('ReactApp/package.json', JSON.stringify(packageJSONBaseConfig, null, 2), (err) => {
+      fs.writeFile(`${answers.name}/package.json`, JSON.stringify(packageJSONBaseConfig, null, 2), (err) => {
         if (err) {
           console.log(err);
         }
 
 
-        shell.cd('ReactApp');
-        const install = shell.exec('npm install && npm start', { async: true });
+        shell.cd(answers.name);
+        const install = shell.exec('npm install', { async: true });
 
         install.stdout.on('data', () => {
           spinner.stop();
@@ -63,38 +72,38 @@ program
 program.parse(process.argv);
 
 
-function copyFileSync( source, target ) {
+function copyFileSync(source, target) {
 
   let targetFile = target;
 
   //if target is a directory a new file with the same name will be created
-  if ( fs.existsSync( target ) ) {
-    if ( fs.lstatSync( target ).isDirectory() ) {
-      targetFile = path.join( target, path.basename( source ) );
+  if (fs.existsSync(target)) {
+    if (fs.lstatSync(target).isDirectory()) {
+      targetFile = path.join(target, path.basename(source));
     }
   }
 
   fs.writeFileSync(targetFile, fs.readFileSync(source));
 }
 
-function copyFolderRecursiveSync( source, target ) {
+function copyFolderRecursiveSync(source, target) {
   let files = [];
 
   //check if folder needs to be created or integrated
-  const targetFolder = path.join( target, path.basename( source ) );
-  if ( !fs.existsSync( targetFolder ) ) {
-    fs.mkdirSync( targetFolder );
+  const targetFolder = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder);
   }
 
-  if ( fs.lstatSync( source ).isDirectory() ) {
-    files = fs.readdirSync( source );
-    files.forEach( function ( file ) {
-      const curSource = path.join( source, file );
-      if ( fs.lstatSync( curSource ).isDirectory() ) {
-        copyFolderRecursiveSync( curSource, targetFolder );
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source);
+    files.forEach(function (file) {
+      const curSource = path.join(source, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
       } else {
-        copyFileSync( curSource, targetFolder );
+        copyFileSync(curSource, targetFolder);
       }
-    } );
+    });
   }
 }
--- questions.js
+++ questions.js
@@ -1,26 +1,35 @@
+const templates = require('./config').templates;
+
 module.exports = [
   {
-    type : 'input',
-    name : 'name',
-    message : 'Enter project name ...',
+    type: 'input',
+    name: 'template',
+    message: `Choose project template: one of [ ${templates.join(' , ')} ]`,
+    default: 'ReactApp',
+    validate: value => templates.includes(value)
+  },
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Enter project name ...',
     default: 'my-app'
   },
   {
-    type : 'input',
-    name : 'description',
-    message : 'Description ...'
+    type: 'input',
+    name: 'description',
+    message: 'Description ...'
   },
   {
-    type : 'input',
-    name : 'version',
-    message : 'Version',
+    type: 'input',
+    name: 'version',
+    message: 'Version',
     default: '1.0.0'
   },
   {
-    type : 'input',
-    name : 'author',
-    message : 'Author'
-  },
+    type: 'input',
+    name: 'author',
+    message: 'Author'
+  }
   // {
   //   type : 'list',
   //   name: 'store',
